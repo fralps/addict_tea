57bdd5d4d94380f391169ca2a4e45030ec3ba0d9
fralps <francois.loupias@orange.fr>
2019-10-07 17:13:52 +0200

    Rubocop launched and it works very well

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,11 @@
+# frozen_string_literal: true
+
 class StaticPagesController < ApplicationController
-	def home
-	end
+  def home; end
 
-	def timer
-	end
+  def timer; end
 
-	def dashboard
-	end
+  def dashboard; end
 
-	def about		
-	end
+  def about; end
 end
