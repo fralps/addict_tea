bc3b719b48c06c2f8a40288ae5df6e1b1ea8c8c5
fralps <francois.loupias@orange.fr>
2019-12-06 16:49:00 +0100

    Remove database cleaner for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,6 @@ require 'colorize'
 # Faker gem
 require 'faker'
 
-# To limit the database entries
-# each time we seed we delete the previous ones and we reset database's ids
-require 'database_cleaner'
-
-DatabaseCleaner.clean_with(:truncation)
-
 User.destroy_all
 Tea.destroy_all
 
